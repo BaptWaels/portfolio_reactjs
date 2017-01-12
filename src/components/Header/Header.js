09ce09210096fd4e029a22d58ea894a82d8b2df2
Baptiste Waels <bwaels@gmail.com>
2017-01-12 12:59:18 +0100

    feat : Add Custom Menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,59 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
+
+import { slide as Menu } from 'react-burger-menu'
+import { Icon } from 'react-mdl'
+
+import Radium from 'radium'
+let RadiumLink = Radium(Link)
+let RadiumIndexLink = Radium(IndexLink)
+
+
 import './Header.scss'
 
-export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
-  </div>
-)
+export const Header = () => {
+  let active = 'home';
+
+  var menuStyles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '36px',
+      height: '30px',
+      right: '36px',
+      top: '36px'
+    },
+    bmBurgerBars: {
+      background: '#BFBFBF'
+    },
+    bmMenu: {
+      background: '#373a47',
+      boxShadow: '10px 0px 20px -10px rgba(0,0,0,0.4)',
+      padding: '60px 1.5em 0'
+    },
+    bmCross: {
+      background: '#b8b7ad'
+    }
+  }
+
+  return (
+  <Menu right noOverlay id={"menu"} className="menu" styles={ menuStyles }>
+    <RadiumIndexLink to='/' className="demo-buttons" activeClassName='route--active'>
+      <Icon name="favorite_border" className="icon" /> Home
+    </RadiumIndexLink>
+
+    <RadiumLink to='/counter' className="demo-buttons" activeClassName='route--active'>
+      <Icon name="face" className="icon" /> About
+    </RadiumLink>
+
+    <RadiumLink to='/projects' className="demo-buttons" activeClassName='route--active'>
+      <Icon name="computer" className="icon" /> Projects
+    </RadiumLink>
+
+    <RadiumLink to='/projects' className="demo-buttons" activeClassName='route--active'>
+      <Icon name="folder_open" className="icon" /> Resume
+    </RadiumLink>
+  </Menu>
+  )
+}
 
 export default Header
