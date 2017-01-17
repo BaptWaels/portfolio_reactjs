328e8eea4f8981a8eee2dda3c88aaa8bb508348f
Baptiste Waels <bwaels@gmail.com>
2017-01-17 15:30:06 +0100

    feat : Add real projects into portfolio

--- src/components/Project/components/Project.js
+++ src/components/Project/components/Project.js
@@ -3,8 +3,6 @@ import React from 'react'
 import { Grid, Cell, Card, CardActions, CardMenu, CardTitle } from 'react-mdl';
 import { browserHistory } from 'react-router';
 
-import VisibilitySensor from 'react-visibility-sensor';
-
 import './Project.scss'
 
 class Project extends React.Component {
@@ -18,10 +16,16 @@ class Project extends React.Component {
 	render() {
     const { project } = this.props;
 
+    var Image = project.imgUrl;
+
+    if(project.img){
+      var Image = require(`./assets/${project.img}`)
+    }
+
     return (
       <Cell className="project" col={4} phone={4} tablet={4} onClick={this.handleClick.bind(this)}>
         <div className="front">
-          <img src={project.imgUrl} className="project-img" />
+          <img src={Image} className="project-img" />
         </div>
 
         <div className="back">
