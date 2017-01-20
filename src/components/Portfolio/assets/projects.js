17303f522937ec5b728dd1bc9a7cd71940ca79cf
Baptiste Waels <bwaels@gmail.com>
2017-01-20 11:29:31 +0100

    feat : Update projects description

--- src/components/Portfolio/assets/projects.js
+++ src/components/Portfolio/assets/projects.js
@@ -1,7 +1,6 @@
 export default [
   {
         name: 'JWaves',
-        url: '/jwaves',
         skills: [{
             name: 'React.js',
             logo: 'reactjs.png'
@@ -16,21 +15,20 @@ export default [
             logo: 'mongodb.png'
         }],
         main_image: 'jwaves.png',
-        images: ['devops.png','jwaves.png','lilly.png', 'parkki.png'],
+        images: [],
         description: {
             client: {
-                title: "Devocité",
-                content: "Devocite est spécialisée dans la réalisation d’objets multimédia spécifiques à destination du grand public. Leur objectif est d’offrir un accès simplifié et ludique à l’information culturelle, citoyenne et pédagogique au plus grand nombre."
+                title: "Hollio",
+                content: "Hollio est une startup qui fourni à ses clients des solutions d'analyse de propagation d'ondes."
             },
             section: {
                 title: "En bref...",
-                content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles."
+                content: "Après une phase de prototypage, Hollio m'a demandé de développer une API sécurisée en Node.js ainsi qu'un backoffice d'administration React.js afin d'intégrer sa solution d'analyse de propagation d'ondes."
             }
         }
     },
     {
         name: 'Pebblo',
-        url: '/pebblo',
         skills: [{
             name: 'React.js',
             logo: 'reactjs.png'
@@ -42,7 +40,7 @@ export default [
             logo: 'leafletjs.png'
         }],
         main_image: 'pebblo.png',
-        images: ['devops.png','jwaves.png','lilly.png', 'parkki.png'],
+        images: ['pebblo_1.png', 'pebblo_3.png', 'pebblo_2.png'],
         description: {
             client: {
                 title: "Devocité",
@@ -50,7 +48,7 @@ export default [
             },
             section: {
                 title: "En bref...",
-                content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles."
+                content: "Dans le but de proposer des visites interactives à ses clients, Devocité m'a demandé de developper un backend d'administration afin de pouvoir configurer ses cartes interactives de façon générique s'appuyant sur leur API REST. Ce backend doit pouvoir configurer tout type de cartes (indoor / outdoor). On peut par exemple ajouter des points d'intérêts avec descriptions multilingues, photos ..."
             }
         }
     },
@@ -71,21 +69,20 @@ export default [
             logo: 'beer.png'
         }],
         main_image: 'parkki.png',
-        images: ['devops.png','jwaves.png','lilly.png', 'parkki.png'],
+        video_url: 'https://www.youtube.com/watch?v=vKWwQQv3Qz0',
         description: {
             client: {
-                title: "Devocité",
-                content: "Devocite est spécialisée dans la réalisation d’objets multimédia spécifiques à destination du grand public. Leur objectif est d’offrir un accès simplifié et ludique à l’information culturelle, citoyenne et pédagogique au plus grand nombre."
+                title: "Parkki",
+                content: "Parkki est une solution permettant d'optimiser le trafic en ville en facilitant le stationnement en voirie. C'est un système de capteurs positionnés sur l'éclairage public ou sur façade qui identifient les places disponibles et permet ainsi aux territoires d'optimiser leurs infrastructures mais aussi aux usagers de se garer au plus vite et au plus près."
             },
             section: {
                 title: "En bref...",
-                content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles."
+                content: "Durant ma dernière année d'études, avec quelques amis, j'ai pu m'investir jour et nuit dans un projet de Startup, Parkki. Ce fut un très beau challenge autant humain que technique. J'ai pu participer aux différents choix techniques pour choisir la stack tech. ainsi qu'à l'élaboration du backoffice. Parkki est depuis incubée à Via-ID, incubateur de Norauto. Je n'ai cependant pas souhaité poursuivre l'aventure pour me diriger vers d'autres horizons tout en gardant cette fibre entrepreneurial. Bon amusement les copains :)"
             }
         }
     },
     {
         name: 'Speaker Meetup Docker',
-        url: '/docker-meetup',
         skills: [{
             name: 'Docker',
             logo: 'docker.png'
@@ -97,21 +94,20 @@ export default [
             logo: 'challenge.png'
         }],
         main_image: 'docker_meetup.png',
-        images: ['devops.png','jwaves.png','lilly.png', 'parkki.png'],
+        video_url: 'https://www.youtube.com/watch?v=0en5mwdMMf4',
         description: {
             client: {
-                title: "Devocité",
-                content: "Devocite est spécialisée dans la réalisation d’objets multimédia spécifiques à destination du grand public. Leur objectif est d’offrir un accès simplifié et ludique à l’information culturelle, citoyenne et pédagogique au plus grand nombre."
+                title: "Docker",
+                content: "Docker est un logiciel libre qui automatise le déploiement d'applications dans des conteneurs logiciels. « Docker est un outil qui peut empaqueter une application et ses dépendances dans un conteneur isolé, qui pourra être exécuté sur n'importe quel serveur Linux » (Docker Inc.)"
             },
             section: {
                 title: "En bref...",
-                content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles."
+                content: "La communauté tech. à Lille étant assez active, de nombreux évènements, conférences sont organisés. Souhaitant m'investir à mon tours et relever un nouveau challenge, j'ai souhaité organiser et présenter Docker lors d'un Meetup. Ce fut une superbe expérience de parler devant 50 à 70 développeurs de la métropole Lilloise."
             }
         }
     },
     {
         name: 'Table connectée',
-        url: '/smart-table',
         skills: [{
             name: 'DIY',
             logo: 'diy.png'
@@ -123,21 +119,20 @@ export default [
             logo: 'javascript.png'
         }],
         main_image: 'lilly.png',
-        images: ['devops.png','jwaves.png','lilly.png', 'parkki.png'],
+        video_url: 'https://www.youtube.com/watch?v=mNjFnTeOv-0',
         description: {
             client: {
-                title: "Devocité",
-                content: "Devocite est spécialisée dans la réalisation d’objets multimédia spécifiques à destination du grand public. Leur objectif est d’offrir un accès simplifié et ludique à l’information culturelle, citoyenne et pédagogique au plus grand nombre."
+                title: "ISEN - Adicode",
+                content: "Les ADICODE® sont un dispositif d’accélérateur d’innovation et de formation pour les élèves-ingénieurs ISEN et les entreprises."
             },
             section: {
                 title: "En bref...",
-                content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles."
+                content: "Durant mes années d'études, j'ai pu réaliser différents projets / POOC. Lilly est l'un d'entre eux. Nous avions pour but de réaliser un projet innovant et technique. Avec plusieurs amis, nous avons déicidé de créer de A à Z une table connectée. J'ai ainsi pu participer à la conception, aux choix techniques ainsi qu'à la réalisation de celle-ci. Etant fan d'IOT, ce projet était vraiment génial à réaliser !"
             }
         }
     },
     {
         name: 'Feedback @ Decathlon',
-        url: '/decath-feedback',
         skills: [{
             name: 'R&D',
             logo: 'creativity.png'
@@ -149,21 +144,19 @@ export default [
             logo: 'rest.png'
         }],
         main_image: 'feedback.png',
-        images: ['devops.png','jwaves.png','lilly.png', 'parkki.png'],
         description: {
             client: {
-                title: "Devocité",
-                content: "Devocite est spécialisée dans la réalisation d’objets multimédia spécifiques à destination du grand public. Leur objectif est d’offrir un accès simplifié et ludique à l’information culturelle, citoyenne et pédagogique au plus grand nombre."
+                title: "Decathlon",
+                content: "Decathlon est un groupe français de grande distribution spécialisé dans les articles de sport et de loisirs."
             },
             section: {
                 title: "En bref...",
-                content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles."
+                content: "Durant mes deux dernières années en alternance, j'ai pu proposer différents sujets de R&D à Décathlon. Je suis partis du constat qu'il était difficile d'avoir des retours client sur nos applications internes. J'ai ainsi pu développer un plugin JS à intégrer dans nos applications afin d'avoir un feedback de la part de nos utilisateurs. Sur ce projet, je me suis chargé de toute la stack (Conception, API Rest, Plugin JS, Interface Admin Angular.js, BDD, configuration du serveur etc.). Ce fut génial de mettre à disposition ce plugin à nos ~500 utilisateurs internes."
             }
         }
     },
     {
         name: 'DevOps @ Decathlon',
-        url: '/decath-devops',
         skills: [{
             name: 'Docker',
             logo: 'docker.png'
@@ -181,15 +174,14 @@ export default [
             logo: 'debian.png'
         }],
         main_image: 'devops.png',
-        images: ['devops.png','jwaves.png','lilly.png', 'parkki.png'],
         description: {
             client: {
-                title: "Devocité",
-                content: "Devocite est spécialisée dans la réalisation d’objets multimédia spécifiques à destination du grand public. Leur objectif est d’offrir un accès simplifié et ludique à l’information culturelle, citoyenne et pédagogique au plus grand nombre."
+                title: "Decathlon",
+                content: "Decathlon est un groupe français de grande distribution spécialisé dans les articles de sport et de loisirs."
             },
             section: {
                 title: "En bref...",
-                content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles."
+                content: "Durant mes deux dernières années en alternance chez Decathlon, ma mission principale était d'automatiser au maximum nos mises en production. J'ai ainsi pu m'amuser avec divers outils pour automatiser les tâches de builds, deploy, emails ... J'ai ainsi customisé un Jenkins, joué énormément avec Docker. J'ai même pu découvrir Bash et sa puissance niveau serveur. Ce fut une très bonne expérience DevOps au sein d'une équipe géniale."
             }
         }
     },
