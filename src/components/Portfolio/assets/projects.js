1433fb1527e29367bfebc2f6a0e49205c8f57f5f
Baptiste Waels <bwaels@gmail.com>
2017-01-31 14:32:36 +0100

    feat : Add english trads on projects

--- src/components/Portfolio/assets/projects.js
+++ src/components/Portfolio/assets/projects.js
@@ -21,17 +21,17 @@ export default [
                 title: "Hollio",
                 content: {
                   fr: "Hollio est une startup qui fournit à ses clients des solutions d'analyse de propagation d'ondes.",
-                  en: ""
+                  en: "Hollio is a french startup which provide to its clients waves propagation analysis."
                 }
             },
             section: {
                 title: {
                   fr: "En bref...",
-                  en: ""
+                  en: "In short..."
                 },
                 content: {
                   fr: "Après une phase de prototypage, Hollio m'a demandé de développer une API sécurisée en Node.js ainsi qu'un backoffice d'administration React.js afin d'intégrer sa solution d'analyse de propagation d'ondes.",
-                  en: ""
+                  en: "After a first prototype, Hollio has asked me to create a NodeJS secured API and a React.js admin interface in order to integrate its waves propagation analysis algorithm."
                 }
             }
         }
@@ -55,17 +55,17 @@ export default [
                 title: "Dévocité",
                 content: {
                   fr: "Dévocité est spécialisée dans la réalisation d’objets multimédia spécifiques à destination du grand public. Leur objectif est d’offrir un accès simplifié et ludique à l’information culturelle, citoyenne et pédagogique au plus grand nombre.",
-                  en: ""
+                  en: "Devocité is specialised in the creation of multimedia objects. Their goal is to simplify cultural access to everyone."
                 }
             },
             section: {
                 title: {
                   fr: "En bref...",
-                  en: ""
+                  en: "In short..."
                 },
                 content: {
                   fr: "Dans le but de proposer des visites interactives à ses clients, Devocité m'a demandé de développer un backend d'administration afin de pouvoir configurer ses cartes interactives de façon générique s'appuyant sur leur API REST. Ce backend doit pouvoir configurer tout type de cartes (indoor / outdoor). On peut par exemple ajouter des points d'intérêts avec descriptions multilingues, photos ...",
-                  en: ""
+                  en: "In order to suggest interactive tours to its customers, Devocité assigned me to develop an admin interface to configure maps with the help of their REST API. This interface is able to configure indoor and outdoor maps using Leaflet.js and React.js."
                 }
             }
         }
@@ -93,17 +93,17 @@ export default [
                 title: "Parkki",
                 content: {
                   fr: "Parkki est une solution permettant d'optimiser le trafic en ville en facilitant le stationnement en voirie. C'est un système de capteurs positionnés sur l'éclairage public ou sur façade qui identifie les places disponibles et permet ainsi aux territoires d'optimiser leurs infrastructures mais aussi aux usagers de se garer au plus vite et au plus près.",
-                  en: ""
+                  en: "Parkki, startup developing a network and sensors detecting free parking slots for a Smart city."
                 }
             },
             section: {
                 title: {
                   fr: "En bref...",
-                  en: ""
+                  en: "In short..."
                 },
                 content: {
                   fr: "Durant ma dernière année d'études, avec quelques amis, j'ai pu m'investir jour et nuit dans un projet de Startup : Parkki. Ce fut un très beau challenge autant humain que technique. J'ai pu participer aux différents choix techniques pour choisir la stack tech. ainsi qu'à l'élaboration du backoffice. Parkki est depuis incubée à Via-ID, incubateur du groupe Mobivia. Je n'ai cependant pas souhaité poursuivre l'aventure pour me diriger vers d'autres horizons tout en gardant cette fibre entrepreneuriale. Plus d'infos sur Parkki.fr",
-                  en: ""
+                  en: "With a bunch of friends, during my last year of studies, I’ve work Days and Nights on a startup project : Parkki. It has been a great human and technical project. I was able to choose with my teammate the tech stack. I’ve also helped them to create the Angular.js admin interface. Parkki is now in a startup accelerator of the Mobivia Group. However, I’ve decided not to follow them. Instead, I’ve created my own company as a Freelance developer. parkki.fr for more informations."
                 }
             }
         }
@@ -127,17 +127,17 @@ export default [
                 title: "Docker",
                 content: {
                   fr: "Docker est un logiciel libre qui automatise le déploiement d'applications dans des conteneurs logiciels. « Docker est un outil qui peut empaqueter une application et ses dépendances dans un conteneur isolé, qui pourra être exécuté sur n'importe quel serveur Linux » (Docker Inc.)",
-                  en: ""
+                  en: "Docker is an open-source project that automates the deployment of applications inside software containers. « Docker containers wrap up a piece of software in a complete filesystem that contains everything it needs to run: code, runtime, system tools, libraries – anything you can install on a server.» (Docker Inc.)"
                 }
             },
             section: {
                 title: {
                   fr: "En bref...",
-                  en: ""
+                  en: "In short..."
                 },
                 content: {
                   fr: "La communauté tech. à Lille étant assez active, de nombreux évènements, conférences sont organisés. Souhaitant m'investir à mon tour et relever un nouveau challenge, j'ai souhaité organiser et présenter Docker lors d'un Meetup. Ce fut une superbe expérience de parler devant 50 à 70 développeurs de la métropole Lilloise.",
-                  en: ""
+                  en: "The tech. community in Lille (FR) is really active. Many events are organised every months. Wishing to invest myself in return and embrace a new challenge, I’ve decided to organise and introduce Docker during a Meetup. It was such a great experience to talk in front of ~60 developers."
                 }
             }
         }
@@ -161,17 +161,17 @@ export default [
                 title: "ISEN - Adicode",
                 content: {
                   fr: "Les ADICODE® sont un dispositif d’accélérateur d’innovation et de formation pour les élèves-ingénieurs ISEN et les entreprises.",
-                  en: ""
+                  en: "The educational, research activities and expertise related to the cooperative development of innovations are brought together under one shared brand: ADICODE®"
                 }
             },
             section: {
                 title: {
                   fr: "En bref...",
-                  en: ""
+                  en: "In short..."
                 },
                 content: {
                   fr: "Durant mes années d'études, j'ai pu réaliser différents projets / POOC. Lilly est l'un d'entre eux. Nous avions pour but de réaliser un projet innovant et technique. Avec plusieurs amis, nous avons décidé de créer de A à Z une table connectée. J'ai ainsi pu participer à la conception, aux choix techniques ainsi qu'à la réalisation de celle-ci. Etant fan d'IOT, ce projet était vraiment génial à réaliser !",
-                  en: ""
+                  en: "I’ve created many projects / POOC during my studies. Lilly was one of them. The main goal of it was to imagine and create an innovative project. With a bunch of friends, we’ve decided to create from scratch a smart table. I’ve had not only the ability to be part in the conception of this table but also the realisation of the technical part. It was a great experiment !"
                 }
             }
         }
@@ -194,17 +194,17 @@ export default [
                 title: "Decathlon",
                 content: {
                   fr: "Decathlon est un groupe français de grande distribution spécialisé dans les articles de sport et de loisirs.",
-                  en: ""
+                  en: "Decathlon is a French brand. It is one of the world's largest sporting goods retailers."
                 }
             },
             section: {
                 title: {
                   fr: "En bref...",
-                  en: ""
+                  en: "In short..."
                 },
                 content: {
                   fr: "Durant mes deux dernières années en alternance, j'ai pu proposer différents sujets de R&D à Décathlon. Je suis parti du constat qu'il était difficile d'avoir des retours clients sur nos applications internes. J'ai ainsi pu développer un plugin JS à intégrer dans nos applications afin d'avoir un feedback de la part de nos utilisateurs. Sur ce projet, je me suis chargé de toute la stack (Conception, API Rest, Plugin JS, Interface Admin Angular.js, BDD, configuration du serveur etc.). Ce fut génial de mettre à disposition ce plugin à nos ~500 utilisateurs internes à l'entreprise.",
-                  en: ""
+                  en: "I’ve done my two last years of studies in apprenticeship at Decathlon. Among other things, I’ve done some R&D. I’ve created a javascript plugin to get user feedback. I was in charge of all the stack on this project (design, REST API, javascript plugin, Angular.js Admin interface, deployment…)."
                 }
             }
         }
@@ -233,17 +233,17 @@ export default [
                 title: "Decathlon",
                 content: {
                   fr: "Decathlon est un groupe français de grande distribution spécialisé dans les articles de sport et de loisirs.",
-                  en: ""
+                  en: "Decathlon is a French brand. It is one of the world's largest sporting goods retailers."
                 }
             },
             section: {
                 title: {
                   fr: "En bref...",
-                  en: ""
+                  en: "In short..."
                 },
                 content: {
                   fr: "Durant mes deux dernières années en alternance chez Decathlon, ma mission principale était d'automatiser au maximum nos mises en production. J'ai ainsi pu m'amuser avec divers outils pour automatiser les tâches de builds, deploy, emails ... J'ai ainsi customisé un Jenkins, joué énormément avec Docker. J'ai même pu découvrir Bash et sa puissance niveau serveur. Ce fut une très bonne expérience DevOps au sein d'une équipe géniale.",
-                  en: ""
+                  en: "I’ve done my two last years of studies in apprenticeship at Decathlon. My main goal during these two years was to automate as possible our deployments in production (automations, deployments, builds, email …). I’ve played a lot with Docker, Jenkins and some Bash scripts."
                 }
             }
         }
